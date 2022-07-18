2fc644e844058e8374a280599c33eb4056c86461
zuli <you@example.com>
2022-07-18 15:02:03 +0600

    hw7

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,43 @@
 import "./App.css";
 import Comment from "./components/Comment/Comment";
-import { comment } from "./data/data";
-
 
 function App() {
+  const comment = [
+    {
+      date: new Date(),
+      text: "Ты не идеален, но, делая ошибки, ты учишься на них. Мне кажется, настоящая сила заключается в этом.",
+      author: {
+        name: "Хината Хьюга",
+        avatarUrl:
+          "http://fc08.deviantart.net/fs71/f/2010/345/d/d/into_the_darkness_by_satokochaaan-d34pm5g.jpg",
+      },
+    },
+    {
+      date: new Date(),
+      text: "Ты не идеален, но, делая ошибки, ты учишься на них. Мне кажется, настоящая сила заключается в этом.",
+      author: {
+        name: "Хината Хьюга",
+        avatarUrl:
+          "http://fc08.deviantart.net/fs71/f/2010/345/d/d/into_the_darkness_by_satokochaaan-d34pm5g.jpg",
+      },
+    },
+    {
+      date: new Date(),
+      text: "Ты не идеален, но, делая ошибки, ты учишься на них. Мне кажется, настоящая сила заключается в этом.",
+      author: {
+        name: "Хината Хьюга",
+        avatarUrl:
+          "http://fc08.deviantart.net/fs71/f/2010/345/d/d/into_the_darkness_by_satokochaaan-d34pm5g.jpg",
+      },
+    },
+  ];
+
   return (
-    <Comment author={comment.author} text={comment.text} date={comment.date} />
+    <div className="appBlock">
+      {comment.map((item) => (
+        <Comment author={item.author} text={item.text} date={item.date} />
+      ))}
+    </div>
   );
 }
 
